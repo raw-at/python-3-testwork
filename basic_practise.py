fd6bfb9fa038764302763ab315adcd9d6cf560a7
Abhinav Rawat <Abhinav Rawat>
2017-06-03 18:25:32 +0530

    print function

---
+++ basic_practise.py
@@ -0,0 +1,22 @@
+print('Hello world!')
+print("We'r going to store");
+print('he said "Hi."');
+ 
+#excape character
+print('we\'r going to the store');
+
+#concatenate with + symbol
+print("hi"+"there");
+
+#concate with , symbol this add a space
+print('hi','there'); 
+
+print('hi',5);
+#string coversion
+print('hi '+str(5));
+
+#int coversion
+print(int('13')+5);
+
+#float conversion
+print(float('8.5')+4);
