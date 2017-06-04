f1beb86078b32ddd45e15ffd46222e9ad326c035
raw-at <hackythemad@gmail.com>
2017-06-04 09:23:19 +0530

    CLass methods static methods and instances methods added

--- oops.py
+++ oops.py
@@ -53,6 +53,73 @@ print(emp_1.__dict__)
 
 print('NUmber of Employee '+str(Employee.no_of_employee));
 
+''' Class Methods ,Regular Methods and statics Methods '''
+
+
+class Student:
+    no_of_students = 0 #class variable
+    raise_marks = 1.04
+
+    def __init__(self,name,last,marks):
+        self.first = name
+        self.last = last
+        self.email = name+'.'+last+'@school.com'
+        self.marks = marks
+        Student.no_of_students += 1;
+
+    def fullname(self):
+        print('{} {}'.format(self.first,self.last))
+    
+    #class method
+
+
+    @classmethod
+    def set_raise_marks(cls,marks):
+        cls.raise_marks = marks
+
+    #Alternative constructor using classmethod
+ 
+    @classmethod
+    def from_string(cls,emp_str):
+        first,last,marks = emp_str.split('-')
+        return cls(first,last,marks)
+    
+    #staticmethods pass nothing
+    @staticmethod
+    
+    def is_workeday(day):
+
+        if(day.weekday() == 5 or day.weekday() == 6 ):
+            return False
+        return True
+
+stud_1 = Student('Abhinav','Rawat',50000);
+stud_2 = Student('Bhanu','Madan',60000);
+    
+Student.set_raise_marks(1.05);
+
+print(Student.raise_marks)
+print(stud_1.raise_marks)
+print(stud_2.raise_marks)
+
+ 
+new_emp_1 = Student.from_string('Abhinav-rawat-25000');
+print(new_emp_1.fullname());
+
+import datetime
+my_date = datetime.date(2016,12,25)
+print(my_date);
+print(Student.is_workeday(my_date));
+
+
+
+
+
+
+
+
+
+
 
 
 
